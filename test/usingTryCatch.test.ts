071f09739f3eea23147df0409098ad172d28fdc8
Renato Oda <renato.oda2@gmail.com>
2021-05-12 10:53:35 -0300

    ci: Correção no comando de coverage

--- test/usingTryCatch.test.ts
+++ test/usingTryCatch.test.ts
@@ -4,7 +4,7 @@ describe('using Try Catch', () => {
   it('Return Data after execution with Promise', async () => {
     const promise = new Promise<string>(resolve => resolve('ola'));
     const result = await usingTryCatch<string>(promise);
-    
+
     expect(result.data).toEqual('ola');
     expect(result.error).toBeUndefined();
   });
@@ -12,7 +12,7 @@ describe('using Try Catch', () => {
   it('Return Error after execution with Promise', async () => {
     const promise = new Promise<string>((_, reject) => reject('ola'));
     const result = await usingTryCatch<string>(promise);
-    
+
     expect(result.data).toBeUndefined();
     expect(result.error).toEqual('ola');
   });
