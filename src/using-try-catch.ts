8b831c149fe95903f84dadc9c14ddf330413681f
Renato Oda <renato.oda2@gmail.com>
2024-12-07 14:19:36 -0300

    update librarys

--- src/using-try-catch.ts
+++ src/using-try-catch.ts
@@ -1,22 +1,16 @@
 interface DataResult<T> {
   data: T | Array<T> | null;
-  error: Error | unknown | null;
+  error: Error | null;
 }
 
 export const usingTryCatch = async <T>(
   promise: Promise<T> | Array<Promise<T>>,
 ): Promise<DataResult<T>> => {
   try {
-    if (Array.isArray(promise)) {
-      return { data: await Promise.all(promise), error: null };
-    }
-
-    return { data: await promise, error: null };
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return { data: null, error: error as Error };
-    }
-    return { data: null, error };
+    const data = Array.isArray(promise) ? await Promise.all(promise) : await promise;
+    return { data, error: null };
+  } catch (error) {
+    return { data: null, error: error instanceof Error ? error : new Error('Unknown error') };
   }
 };
 
